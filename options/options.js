90f3652409e6351c1f0a893874eb7018559db788
Crowdbotics <engineering@crowdbotics.com>
2023-06-29 15:32:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "bold-forest-dev-95995",
+  url: "https://bold-forest-dev-95995.botics.co",
+  api: "https://bold-forest-dev-95995.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
